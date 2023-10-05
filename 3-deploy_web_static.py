b84eafcb3b82f402381bcadf4685e7d97c119199
KSI5 <krissigebs@gmail.com>
2023-10-05 04:08:53 -0700

    Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["52.3.246.137", "3.84.222.135"]
 
 
 def do_pack():
