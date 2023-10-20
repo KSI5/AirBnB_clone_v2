fde5e6ac1162135e9e478a0e6160ef64850dd794
KSI5 <krissigebs@gmail.com>
2023-10-20 10:59:48 +0000

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,43 +1,31 @@
 #!/usr/bin/python3
-'''
-A simple Flask web application.
+'''A simple Flask web application.
 '''
 from flask import Flask
-from markupsafe import escape
+
 
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
-def hello_hbnb():
-    '''
-    The home page.
-    '''
+def index():
+    '''The home page.'''
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    '''
-    The hbnb page.
-    '''
+    '''The hbnb page.'''
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_page(text):
-    '''
-    The c page.
-    '''
-    text = text.replace('_', ' ')
-    return 'C {}'.format(escape(text))
+    '''The c page.'''
+    return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_page(text='is_cool'):
-    '''
-    The python page.
-    '''
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(escape(text))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
